848889c49a99e75455ca44927959be7811a6c2b5
Claudiu-IonutCristea <claudiucristea12@gmail.com>
2023-03-02 17:37:17 +0200

    Add measure unit for SyncPeriod

--- SDET_Team_Task.FolderSync/ProgramArguments.cs
+++ SDET_Team_Task.FolderSync/ProgramArguments.cs
@@ -4,7 +4,7 @@ internal class ProgramArguments
 	public string SourceFolderPath { get; set; } = Directory.GetCurrentDirectory() + "\\source";
 	public string ReplicaFolderPath { get; set; } = Directory.GetCurrentDirectory() + "\\replica";
 	public string LogFilePath { get; set; } = Directory.GetCurrentDirectory() + "\\log.txt";
-	public int SyncPeriod { get; set; } = 10_000;
+	public int SyncPeriodMs { get; set; } = 10_000; //10 seconds
 	public bool LogTreeView { get; set; } = false;
 
 }
