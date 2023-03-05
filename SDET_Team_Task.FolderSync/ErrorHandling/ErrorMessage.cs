9505c145727d0b94f5fe4a7d5ccd4a6d9d4c6d38
Claudiu-IonutCristea <claudiucristea12@gmail.com>
2023-03-06 00:08:20 +0200

    Add ErrorMessage.cs

---
+++ SDET_Team_Task.FolderSync/ErrorHandling/ErrorMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SDET_Team_Task.FolderSync.ErrorHandling;
+internal class ErrorMessage
+{
+	[JsonPropertyName("id")]
+	public int ErrorId { get; set; }
+	[JsonPropertyName("message")]
+	public string Message { get; set; } = "";
+}
