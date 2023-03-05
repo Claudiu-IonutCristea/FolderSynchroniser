35b2370dc27661e85fce585fcdbaffe7eea651a0
Claudiu-IonutCristea <claudiucristea12@gmail.com>
2023-03-06 00:08:10 +0200

    Add WriteAllErrorsAsync Method

--- SDET_Team_Task.FolderSync/ErrorHandling/ErrorsManager.cs
+++ SDET_Team_Task.FolderSync/ErrorHandling/ErrorsManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace SDET_Team_Task.FolderSync.ErrorHandling;
 internal static class ErrorsManager
@@ -17,6 +18,40 @@ internal static class ErrorsManager
     {
         _errors.Add(new() { ErrorCode = errorCode, ErrorSource = errorSourceName });
     }
+
+	public static void Add(Error error)
+	{
+		_errors.Add(error);
+	}
+
+	public static async Task<string> WriteAllErrorsAsync()
+	{
+		var errorsData = await File.ReadAllTextAsync(MESSAGES_PATH);
+		var errorMessages = JsonSerializer.Deserialize<IEnumerable<ErrorMessage>>(errorsData);
+		var result = new StringBuilder();
+
+		if(errorMessages == null)
+			throw new Exception("Json file could not be deserialized!");
+
+		foreach(var error in _errors)
+		{
+			//find error message that matches error code
+			var errMessage = errorMessages.FirstOrDefault(e => e.ErrorId == error.ErrorCode);
+
+			//in case error message is not found
+			if(errMessage == null)
+			{
+				result.AppendLine("Unknown error");
+				continue;
+			}
+
+			var message = errMessage.Message.Replace("#", error.ErrorSource);
+
+			result.AppendLine(message);
+		}
+
+		return result.ToString();
+	}
 
     //Error categories are in multiples of 100
     //check if there are any errors with code between x01 and x99
